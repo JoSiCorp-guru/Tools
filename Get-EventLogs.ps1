aded613ebce34926c79b43902651d2845c9dab95
JoSiCorp-guru <80458433+JoSiCorp-guru@users.noreply.github.com>
2021-03-11 13:00:29 +0200

    Add files via upload

---
+++ Get-EventLogs.ps1
@@ -0,0 +1,16 @@
+﻿# Config
+$logFileNameApp = "Application"
+$logFileNameSec = "Security"
+$logFileNameSys = "System" 
+$path = "$pwd\"
+ 
+# do not edit
+$exportFileName = $logFileNameApp + (hostname) + (get-date -f yyyyMMdd) + ".evt"
+$logFile = Get-WmiObject Win32_NTEventlogFile | Where-Object {$_.logfilename -eq $logFileNameApp}
+$logFile.backupeventlog($path + $exportFileName)
+$exportFileName = $logFileNameSec + (hostname) + (get-date -f yyyyMMdd) + ".evt"
+$logFile = Get-WmiObject Win32_NTEventlogFile | Where-Object {$_.logfilename -eq $logFileNameSec}
+$logFile.backupeventlog($path + $exportFileName)
+$exportFileName = $logFileNameSys + (hostname) + (get-date -f yyyyMMdd) + ".evt"
+$logFile = Get-WmiObject Win32_NTEventlogFile | Where-Object {$_.logfilename -eq $logFileNameSys}
+$logFile.backupeventlog($path + $exportFileName)
